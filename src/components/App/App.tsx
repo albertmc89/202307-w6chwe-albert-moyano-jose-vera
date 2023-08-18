fb4635b671f2fc4d45e946a730c24fb9a65a57fb
Albert Colubi <albertmc89@gmail.com>
2023-08-18 20:09:20 +0200

    Modify App component and add route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
 const App = (): React.ReactElement => {
-  return <p>Hello world!</p>;
+  return (
+    <div className="container">
+      <h1>Serial Friends</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
