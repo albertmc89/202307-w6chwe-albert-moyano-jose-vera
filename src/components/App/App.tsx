46b1f2d9ce48233761c3ab43b9fcabab9fe02858
Albert Colubi <albertmc89@gmail.com>
2023-08-18 21:02:21 +0200

    Add Navigation component styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
       <h1>Serial Friends</h1>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
       </Routes>
