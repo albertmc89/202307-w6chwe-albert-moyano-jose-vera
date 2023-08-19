e48418ec26b7c5d0aaa534852159b21081e076f1
Albert Colubi <albertmc89@gmail.com>
2023-08-19 18:38:49 +0200

    Test toggleUsers reducer from usersSlice

--- src/store/Users/usersReducer.test.tsx
+++ src/store/Users/usersReducer.test.tsx
@@ -1,6 +1,10 @@
 import { usersMockApi } from "../../mocks/mocksData";
 import { UserState } from "../types";
-import { loadUsersActionCreator, usersReducer } from "./UsersSlice";
+import {
+  loadUsersActionCreator,
+  toggleUserActionCreator,
+  usersReducer,
+} from "./UsersSlice";
 
 describe("Given a usersReducer reducer", () => {
   describe("When it receives a load users action with two users", () => {
@@ -15,6 +19,28 @@ describe("Given a usersReducer reducer", () => {
       const newUserState = usersReducer(currentUserState, loadUsersAction);
 
       expect(newUserState).toHaveProperty("users", users);
+    });
+  });
+
+  describe("When it receives a state with 2 users and a toggleUser action with id 1", () => {
+    test("Then it should return a new state with 2 user and the property 'isFriend' of the user with id '1' changed", () => {
+      const currentUsersState: UserState = {
+        users: usersMockApi,
+      };
+
+      const idToToggle = 1;
+
+      const toggleIsFriendAction = toggleUserActionCreator(idToToggle);
+      const newUsersState = usersReducer(
+        currentUsersState,
+        toggleIsFriendAction,
+      );
+
+      const toggledUser = newUsersState.users.find(
+        (users) => users.id === idToToggle,
+      );
+
+      expect(toggledUser).toHaveProperty("isFriend", false);
     });
   });
 });
