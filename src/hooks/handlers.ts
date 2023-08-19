95544411e3e74f9ced84ea26514d246b695bdb5a
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-19 13:59:30 +0200

    Add handlers for test the customHook

---
+++ src/hooks/handlers.ts
@@ -0,0 +1,14 @@
+import { rest } from "msw";
+import { usersMock } from "../mocks/mocksData";
+
+export const handlers = [
+  rest.get(`${import.meta.env.VITE_API_PEOPLE_URL}people`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(usersMock));
+  }),
+];
+
+export const errorHandlers = [
+  rest.get(`${import.meta.env.VITE_API_PEOPLE_URL}people`, (_req, res, ctx) => {
+    return res(ctx.status(404, "can't get any user"));
+  }),
+];
