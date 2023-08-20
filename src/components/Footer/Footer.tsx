37d96d9e37536eb2d483e5f0e18b5fe4846fb24e
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-20 21:01:45 +0200

    Add footer on App component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = (): React.ReactElement => {
+  return (
+    <footer className="footer">
+      <div className="footer__info">
+        <span className="info__company">Serial Friends S.L.</span>
+        <span className="info__adress">C/ Diputació 37, Barcelona</span>
+        <span className="info__phone">+34 651 447 892</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
