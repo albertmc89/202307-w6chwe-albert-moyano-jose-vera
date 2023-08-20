509b9e060f49254c1e8d9404335810e3a1707110
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-20 13:39:31 +0200

    Create homepage and useAppDispatch and useEffect functions from App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,18 @@
-import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import usePeopleApi from "../../hooks/usePeopleApi";
-import { useAppDispatch } from "../../store";
-import { loadUsersActionCreator } from "../../store/Users/UsersSlice";
+import UsersListPage from "../../pages/UsersListPage/UsersListPage";
 import Header from "../Header/Header";
-import UsersList from "../UsersList/UsersList";
 import "./App.css";
 
 const App = (): React.ReactElement => {
-  const dispatch = useAppDispatch();
-  const { getUsers } = usePeopleApi();
-
-  useEffect(() => {
-    (async () => {
-      const users = await getUsers();
-
-      dispatch(loadUsersActionCreator(users));
-    })();
-  }, [getUsers, dispatch]);
-
   return (
     <div className="container">
       <Header />
       <main className="main-content">
         <Routes>
+          <Route path="/home" element={<UsersListPage />} />
           <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="newrelationshippage" element={<Navigate to="/home" />} />
         </Routes>
-        <UsersList />
       </main>
     </div>
   );
