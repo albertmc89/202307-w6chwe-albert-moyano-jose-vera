8a665f9a6e72f5599225fb880155149082943f94
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-20 21:55:50 +0200

    Modify css on Footer Component

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Footer from "./Footer";
 
 describe("Given a Footer component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a name company, street and phone number", () => {
       const companyTest = "company";
-      const streetTest = "company";
-      const phoneTest = "company";
+      const streetTest = "street";
+      const phoneTest = "phone";
 
-      render(<Footer />);
+      render(
+        <BrowserRouter>
+          <Footer />
+        </BrowserRouter>,
+      );
 
       const footerCompanyText = screen.getByLabelText(companyTest);
       const footerStreetText = screen.getByLabelText(streetTest);
@@ -17,6 +22,27 @@ describe("Given a Footer component", () => {
       expect(footerCompanyText).toBeInTheDocument();
       expect(footerStreetText).toBeInTheDocument();
       expect(footerPhoneText).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Given a Footer component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should show the links 'Home' and 'Add new relationship'", () => {
+      const navHome = "Home";
+      const navAdd = "Add new relationship";
+
+      render(
+        <BrowserRouter>
+          <Footer />
+        </BrowserRouter>,
+      );
+
+      const navLinkHome = screen.getByRole("link", { name: navHome });
+      const navLinkAdd = screen.getByRole("link", { name: navAdd });
+
+      expect(navLinkHome).toBeInTheDocument();
+      expect(navLinkAdd).toBeInTheDocument();
     });
   });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+import { NavLink } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = (): React.ReactElement => {
   return (
     <footer className="footer">
+      <ul className="footer-navigation">
+        <li>
+          <NavLink className="navigation__home" to="/home">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navigation__create" to="/NewRelationshipPage">
+            Add new relationship
+          </NavLink>
+        </li>
+      </ul>
       <div className="footer__info">
         <span aria-label="company">Serial Friends S.L.</span>
         <span aria-label="street">C/ Diputació 37, Barcelona</span>
